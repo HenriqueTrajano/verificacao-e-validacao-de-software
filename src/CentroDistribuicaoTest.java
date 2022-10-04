58a1393c2456030d82ad88baba6375fe078e9592
lucasdellatorre <lucasdellatorre222@gmail.com>
2022-10-03 22:58:20 -0300

    chore: initial template for test class

---
+++ src/CentroDistribuicaoTest.java
@@ -0,0 +1,51 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class CentroDistribuicaoTest {
+    private CentroDistribuicao cd;
+    @BeforeEach
+    void setUp() {
+        cd = new CentroDistribuicao();
+
+    }
+
+    @Test
+    void defineSituacao() {
+    }
+
+    @Test
+    void getSituacao() {
+    }
+
+    @Test
+    void gettAditivo() {
+    }
+
+    @Test
+    void gettGasolina() {
+    }
+
+    @Test
+    void gettAlcool1() {
+    }
+
+    @Test
+    void gettAlcool2() {
+    }
+
+    @Test
+    void recebeAditivo() {
+    }
+
+    @Test
+    void recebeGasolina() {
+    }
+
+    @Test
+    void recebeAlcool() {
+    }
+
+    @Test
+    void encomendaCombustivel() {
+    }
+}
